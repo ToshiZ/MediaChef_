760470d2a1e65f2473e325284625edb8f3ba2d6e
ToshiZ <andi4andi@gmail.com>
2014-05-26 05:30:58 +0400

    time start

--- MediaChef_/Scripts/editor-script.js
+++ MediaChef_/Scripts/editor-script.js
@@ -1,38 +1,56 @@
 jQuery.noConflict();
 jQuery(document).ready(function ($) {
-        ////$('.draggable').height(function () { return $('#con').height() * 0.1 });
-       $('.draggable').draggable({ containment: "#con", scroll: false });
+    ////$('.draggable').height(function () { return $('#con').height() * 0.1 });
+    var video = $('video').get(0);
+    var canvas = $("#canvas-prewatch");
+    var context = canvas[0].getContext('2d');
+    var page_h = $(window).height();
+    var page_w = $(window).width();
+    var positionTmp;
 
-        var video = $('video').get(0);        
-        var canvas = $("#canvas-prewatch");
-        
-        var context = canvas[0].getContext('2d');
-        
-        
+    $('.draggable').draggable({
+        containment: "#con",
+        scroll: false,
+        stack: '.draggable',
+        drag: function () {
+            positionTmp = $(this).offset().left;
+            $(this).attr('time-start', positionTmp);
+            $(this).find('span').text(positionTmp);
+        }
+    });
+  // назначает положение объектов
+    $('.draggable').each(function () {
+        var pos = $(this).offset().left;
+        $(this).attr('time-start', pos);
+        $(this).find('span').text(pos);
+    });
+    $('.draggable.video, .draggable.design').each(function () {
+        //var dur = $(this)[0].duration;
+        //alert(dur);
+        alert(parseInt($(this).get(0).duration));
 
-        $("#canvas").css('width', '300').css('height', '200');
 
-        var cw = Math.floor(canvas.clientWidth);
-        var ch = Math.floor(canvas.clientHeight);
-        canvas[0].width = 300;
-        canvas[0].height = 200;
-        //if (canvas[0].getContext('2d')) { alert('1'); }
+    });
+
+
+               
+        $("#canvas").css('width', page_w).css('height', page_h);
+        canvas[0].width = page_w;
+        canvas[0].height = page_h;      
         context.fillRect(10, 20, 150, 100);
         
-        //alert('1123');
-        
-
         $('#prewatch-button').click( function () {           
             video.play();
             var d = draw();
-            var loop = setInterval(draw, 30);
+            var loop = setInterval(draw, 20);
 
-        });
-        function draw() {
+        });        
+        function draw() {                      
             if (video.paused || video.ended) return false;
-            context.drawImage(video, 0, 0, 300, 200);        
+            context.drawImage(video, 0, 0);
                  
-        }       
+        }
+        
 
     });
 
