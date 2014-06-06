80913e7b4b2be781e6bc752d5c6b6022545f704e
ToshiZ <andi4andi@gmail.com>
2014-06-07 03:04:22 +0400

    norm timers

--- MediaChef_/Scripts/editor-script.js
+++ MediaChef_/Scripts/editor-script.js
@@ -15,121 +15,80 @@ jQuery(document).ready(function ($) {
         stack: '.draggable',
         drag: function () {
             positionTmp = $(this).offset().left/12;
-            $(this).data('time-start', positionTmp);
+            $(this).attr('data-time-start', positionTmp);
             $(this).find('.time-counter').text(positionTmp);
         }
     });
 
-    $('.droppable').droppable({
-        drop: function (event, ui) {
+    //$('.droppable').droppable({
+    //    drop: function (event, ui) {
          
-        },
-        over: function (event, ui) {
-            var draggableElement = ui.draggable;                     
-            var mergeT;            
+    //    },
+    //    over: function (event, ui) {
+    //        var draggableElement = ui.draggable;                     
+    //        var mergeT;            
           
-            if (draggableElement.data('time-start') > $(this).data('time-start')) {
-                //alert('1');
-                mergeT = $(this).data('time-end') * 12 + $(this).data('time-start') - draggableElement.data('time-start');
-               // alert('2');
-            }
-            else {
-               // alert('3');
-                mergeT = draggableElement.data('time-end') * 12 + draggableElement.data('time-start') - $(this).data('time-start');
-               // alert('4');
-            }
-            draggableElement.find('.merge-time').text(mergeT);
-            //alert('5');
-        },
-        out: function (event, ui) {        
-        }
-    });
+    //        if (draggableElement.data('time-start') > $(this).data('time-start')) {
+    //            //alert('1');
+    //            mergeT = $(this).data('time-end') * 12 + $(this).data('time-start') - draggableElement.data('time-start');
+    //           // alert('2');
+    //        }
+    //        else {
+    //           // alert('3');
+    //            mergeT = draggableElement.data('time-end') * 12 + draggableElement.data('time-start') - $(this).data('time-start');
+    //           // alert('4');
+    //        }
+    //        draggableElement.find('.merge-time').text(mergeT);
+    //        //alert('5');
+    //    },
+    //    out: function (event, ui) {        
+    //    }
+    //});
   
   // назначает положение объектов
     $('.draggable').each(function () {
-        var pos = ($(this).offset().left / 12); 
-        $(this).data('time-start', pos);
-        $(this).find('span').text(pos);
-    });
-    
+        positionTmp = ($(this).offset().left / 12);
+        $(this).attr('data-time-start', positionTmp);
+        $(this).find('span').text(positionTmp);
+    });   
   
     $('video, audio').each(function () {
         $(this).on('loadedmetadata', function () {
             var dur = $(this)[0].duration;
             $(this).parent('li').attr('data-time-end', dur);
-
-            $(this).parent('li').width(parseInt(dur*12));
+           // $(this).parent('li').width(parseInt(dur*12));
             });            
     }); 
-
         canvas[0].width = page_w;
         canvas[0].height = page_h;  
-        
-       /* $('#prewatch-button').click( function () {           
-           // video.play();
-            $('video').each(function () {
-                $(this).get(0).play();
-            });
-            //var timers = [];
-            //$('video').each( function (i) {
-            //    var tm = $(this).parent('li').data('time-start');                
-            //    timers[i] = setTimeout(function () { $(this).get(0).play(); }, tm*1000);
-            //    var d = draw();
-            //    var loop = setInterval(draw, 20);
-            //   });
-            var d = draw();
-           // var loop = setInterval(draw, 20);
-
-        });        
-        function draw() {                      
-            if (video.paused || video.ended) return false;
-            context.globalAlpha = 0.5;
-            $('video').each(function () {
-                context.drawImage($(this)[0], 0, 0);
-            });
-            setTimeout(draw, 20);
-           // context.drawImage(video, 0, 0);                 
-        }    */
+      
         var mediaQueue = [];
-       // var nowPlaying;
-
-        //$('#prewatch-button').click(function () {
-        //    // video.play();
-        //    nowPlaying = 0;
-        //    $('video, audio').each(function (i) {
-        //        var startTime = $(this).parent('li').data('time-start');
-        //        mediaQueue[i] = $(this).get(0);
-        //        setTimeout(function () {
-        //            mediaQueue[i].play();                    
-        //            if (mediaQueue[i].tagName != 'AUDIO') {
-        //                draw(mediaQueue[i]);
-        //            }
-        //        }, startTime * 1000);
-               
-        //    });    
-        //});
+        var playingCount;
 
         $('#prewatch-button').click(playByTimer);
-        function playByTimer () {
-            //nowPlaying = 0;
+
+        function playByTimer() {
             var startTime = [];
+            var timeOuts = [];
+            timeOuts.forEach(function (it) {
+                clearTimeout(it);
+            });
             var start = new Date().getTime(),
                 timeCount = 0,
                 real,
                 offset = 0,
                 dif = 0;
+            playingCount = 0;
             
-            $('video, audio').each(function (i) {
-
-              
-                startTime[i] = $(this).parent('li').data('time-start');
+            $('video, audio, img').each(function (i) {
+                startTime[i] = $(this).parent('li').attr('data-time-start');
                 mediaQueue[i] = $(this);
             });
 
             mediaQueue.sort(function (a, b) {
-                if (a.parent('li').data('time-start') > b.parent('li').data('time-start'))
+                if (a.parent('li').attr('data-time-start') > b.parent('li').attr('data-time-start'))
                     return 1;
-                if (a.parent('li').data('time-start') < b.parent('li').data('time-start'))
+                if (a.parent('li').attr('data-time-start') < b.parent('li').attr('data-time-start'))
                     return -1;
                 return 0;
             });
@@ -146,60 +105,41 @@ jQuery(document).ready(function ($) {
                     var nowPlaying = mediaQueue.filter(function (element, index) {
                         return (index <= i && index >= i - offset);
                     });
-                    offset = 0;
+                    offset = 0;            
                     real = new Date().getTime();
                     dif = real - start;
-                    setTimeout(function () {
+
+                    timeOuts[i] = setTimeout(function () {
                         nowPlaying.forEach(function (it) {
                             it.get(0).play();
+                            playingCount++;
+                            context.globalAlpha = (playingCount > 1) ? 0.5 : 1;
+                           // if()
+                            draw(nowPlaying);
+                            $('#test').text("it  " + item * 1000 + "dif  " + dif + "time   " + timeCount);
+                            timeCount += item * 1000;
                         });
-                    }, item * 1000 - dif - timeCount);
-                    timeCount += item;
-                    alert(timeCount);
+                    }, item * 1000 - dif - timeCount);                    
                 }
             });
-            
-            //alert('1');
-            //alert(startTime);
-            //alert(arr);
-            //alert(real);
-            //alert(start);
-            //alert(dif);
-            //alert(str1);
-           
-            //setTimeout(function () {
-            //    mediaQueue[i].play();
-            //    if (mediaQueue[i].tagName != 'AUDIO') {
-            //        draw(mediaQueue[i]);
-            //    }
-            //}, startTime * 1000);
         }
+        var drawTimeOuts = [];       
         function draw(media) {
-            if (media.paused || media.ended) {
-                alert('end');
-                context.clearRect(0, 0, canvas.width, canvas.height);
-                nowPlaying--;
-                return false;
-            }
-            if (nowPlaying > 1)
-                context.globalAlpha = 0.5;
-            else
-                context.globalAlpha = 1;
-            // $('video').each(function () {
-           // if (!media.paused || !media.ended)
-                context.drawImage(media, 0, 0);
-            // });
-                nowPlaying++;
-            setTimeout(draw, 20, media);
-            // context.drawImage(video, 0, 0);                 
-        }
-        function sortByTimeStart(a,b){
-            if(a.parent('li').data('time-start') > b.parent('li').data('time-start'))
-                return 1;
-            if (a.parent('li').data('time-start') < b.parent('li').data('time-start'))
-                return -1;
-            return 0;
-        }
+            media.forEach(function(m){
+                m.get(0).addEventListener('ended', function () {
+                    clearTimeout(drawTimeOuts[m.parent('li').attr('data-time-start')]);
+                    context.clearRect(0, 0, canvas.width, canvas.height);
+                    playingCount--;
+                    context.globalAlpha = (playingCount > 1) ? 0.5 : 1;
+                });
+            });
+            
+            media.forEach(function (m, it) {
+                context.drawImage(m[0], 0, 0);
+                $('#test').text(" media  " + m + "count  " + playingCount );
+                drawTimeOuts[m.parent('li').attr('data-time-start')] = setTimeout(draw, 20, media);
+            });            
+        }        
     });    
 
 
